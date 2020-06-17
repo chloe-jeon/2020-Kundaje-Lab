b91fbcc10f4a35c374cd3f02a987d95cae9e4102
cutechloej@hotmail.com <kateosipovav@gmail.com>
2020-06-16 17:05:40 -0700

    created windows for protein coding genes

---
+++ test.sh
@@ -0,0 +1,9 @@
+chloejeon$ awk -v OFS="\t" '{ if ($3 == "gene") print $1, $4 - 1, $5}' gencode.v34.annotation.gtf > all_genes.bed
+open all_genes.bed
+cat gencode.v34.annotation.gtf | grep "protein_coding" > protein_coding.gtf
+awk -v OFS="\t" '{ if ($3 == "gene") print $1, $4 - 1, $5}' protein_coding.gtf > protein_coding_genes.bed
+open protein_coding_genes.bed
+bedtools makewindows -b protein_coding_genes.bed -w 200 -s 100 > protein_coding_gene_windows.bed
+sort -k 1,1 -k2,2n protein_coding_gene_windows.bed > pro_cod_gene_windows_sorted.bed
+bedtools merge -i pro_cod_gene_windows_sorted.bed | head
+cat protein_coding_genes.bed | head
